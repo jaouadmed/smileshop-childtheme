e8cc445bbbf1ad5afe666d60d727e2255a540d67
jaouadmed <jaouadmohamed0@gmail.com>
2020-11-12 01:58:37 +0100

    toggle 2

--- assets/js/custom-lib.js
+++ assets/js/custom-lib.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var xsmall = 991;
     
     function showMobileLogo() {
-        var desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
+        desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
         if (document.body.clientWidth <= xsmall) { //for mobile view
             if(desk.is(':visible')) desk.toggle();
             if(!mob.is(':visible')) mob.toggle();
