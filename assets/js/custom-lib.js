e032b9c21b78088b1c180c687da5fe98ef21b087
jaouadmed <jaouadmohamed0@gmail.com>
2020-11-12 13:01:00 +0100

    trigger resize on load 3 var

--- assets/js/custom-lib.js
+++ assets/js/custom-lib.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var xsmall = 414;
     function showMobile() {
-        var desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
+        desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
         if (document.body.clientWidth <= xsmall) { //for mobile view
             if(desk.is(':visible')) desk.toggle();
             if(!mob.is(':visible')) mob.toggle();
