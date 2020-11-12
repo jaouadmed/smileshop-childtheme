1f80470a7055fcd9c8ddea0f57fe6fc5ff072884
jaouadmed <jaouadmohamed0@gmail.com>
2020-11-12 11:31:15 +0100

    toggle js edit final

--- assets/js/custom-lib.js
+++ assets/js/custom-lib.js
@@ -1,5 +1,9 @@
-$(document).ready(function() {
+$(document).load(function() {
     var xsmall = 414;
+    if (document.body.clientWidth <= xsmall) { //for mobile view
+        if(desk.is(':visible')) desk.toggle();
+        if(!mob.is(':visible')) mob.toggle();
+    }
     $(window).resize(function() {
             var desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
             if (document.body.clientWidth <= xsmall) { //for mobile view
