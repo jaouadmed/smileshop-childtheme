941c64c3fb953c1a0c797caf5c3f216391e13004
jaouadmed <jaouadmohamed0@gmail.com>
2020-11-12 11:50:01 +0100

    trigger resize

--- assets/js/custom-lib.js
+++ assets/js/custom-lib.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
     var xsmall = 414;
-    var desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
-    if (document.body.clientWidth <= xsmall) { //for mobile view
-        if(desk.is(':visible')) desk.toggle();
-        if(!mob.is(':visible')) mob.toggle();
-    }
+    
     $(window).resize(function() {
-            if (document.body.clientWidth <= xsmall) { //for mobile view
-                if(desk.is(':visible')) desk.toggle();
-                if(!mob.is(':visible')) mob.toggle();
-            } else { //for desktop view
-                if(!desk.is(':visible')) desk.toggle();
-                if(mob.is(':visible')) mob.toggle();
-            }
+        var desk = $("img.logo.img-responsive:first-child"), mob = $("img.logo.img-responsive:last-child");
+        if (document.body.clientWidth <= xsmall) { //for mobile view
+            if(desk.is(':visible')) desk.toggle();
+            if(!mob.is(':visible')) mob.toggle();
+        } else { //for desktop view
+            if(!desk.is(':visible')) desk.toggle();
+            if(mob.is(':visible')) mob.toggle();
+        }
     });
+    $(window).trigger('resize');
 });
