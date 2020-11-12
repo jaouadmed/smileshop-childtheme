1c18ad5dfc7a72e458aa1997cb053fa205f406ef
jaouadmed <jaouadmohamed0@gmail.com>
2020-11-12 01:39:04 +0100

    toggle

--- assets/js/custom-lib.js
+++ assets/js/custom-lib.js
@@ -1,26 +1,17 @@
-
-function showView() {
-    if (document.body.clientWidth <= mobileViewSize) { //for mobile view
-        $("#tvcmsdesktop-logo img.logo").attr("style", "object-fit: none; object-position: 0 0; width: 104px; height: 104px;");
-        moveDataInMobileView('#tvcmsdesktop-logo', '#tvcmsmobile-header-logo');
-        
-        moveDataInMobileView('#_desktop_cart', '#tvmobile-cart');
-        moveDataInMobileView('#tvcmsdesktop-account-button', '#tvcmsmobile-account-button');
-        moveDataInMobileView('#tvdesktop-megamenu', '#tvmobile-megamenu');
-        // moveDataInMobileView('#tvcmsdesktop-vertical-menu', '#tvcmsmobile-vertical-menu');
-        moveDataInMobileView('#_desktop_search', '#tvcmsmobile-search');
-        // moveDataInMobileView('.tvsearch-header-display-wrappper', '#tvcmsmobile-vertical-menu');
-        //console.log('moveDataInMobileView');
-    } else { //for desktop view
-        
-        moveDataInDesktopView('#tvcmsdesktop-logo', '#tvcmsmobile-header-logo');
-        $("#tvcmsdesktop-logo img.logo").removeAttr("style");
-
-        moveDataInDesktopView('#_desktop_cart', '#tvmobile-cart');
-        moveDataInDesktopView('#tvcmsdesktop-account-button', '#tvcmsmobile-account-button');
-        moveDataInDesktopView('#tvdesktop-megamenu', '#tvmobile-megamenu');
-        // moveDataInDesktopView('#tvcmsdesktop-vertical-menu', '#tvcmsmobile-vertical-menu');
-        moveDataInDesktopView('#_desktop_search', '#tvcmsmobile-search');
-        // console.log('moveDataInDesktopView');
+$(document).ready(function() {
+    var xsmall = 991;
+    
+    function showMobileLogo() {
+        if (document.body.clientWidth <= xsmall) { //for mobile view
+            $("img.logo.img-responsive:first-child").toggle();
+            $("img.logo.img-responsive:last-child").toggle();
+        } else { //for desktop view
+            $("img.logo.img-responsive:first-child").toggle();
+            $("img.logo.img-responsive:last-child").toggle();
+        }
     }
-} //showView
+
+    $(window).resize(function() {
+        showMobileLogo();
+    });
+}
