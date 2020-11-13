bc934200b708642c3ec7c363b83234582db74f62
jaouadmed <jaouadmohamed0@gmail.com>
2020-11-13 17:15:23 +0100

    click outside and remove megamaenu

--- assets/js/custom-lib.js
+++ assets/js/custom-lib.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
     });
     showMobile();
 
-    $('body').on('click', function() {
+    $('body').not(".menu-content").on('click, ', function() {
         if ($('.menu-content').hasClass('open')) {
             $('.menu-content').removeClass('open');
             $('.title-menu-mobile').removeClass('open');
